96db7a89264fb65d50f3773dfe8a263537929fba
jammees <86011536+jammees@users.noreply.github.com>
2026-01-10 22:34:27 +0100

    Start reworking examples

---
+++ Libraries/PostprocessableRazorPanel/Code/Examples/Constants.cs
@@ -0,0 +1,31 @@
+﻿namespace PostprocessPanel.Examples;
+
+public static class Constants
+{
+	public static string ShadersPath => "example shaders";
+
+	public enum ComputeTypes
+	{
+		/// <summary>
+		/// Nothing interesting happens.
+		/// </summary>
+		None,
+
+		/// <summary>
+		/// Dissolve the texture using the slider.
+		/// </summary>
+		Dissolve,
+
+		/// <summary>
+		/// Static, shows basic texture
+		/// modification.
+		/// </summary>
+		OnlyRedGreen,
+		
+		/// <summary>
+		/// Applies a crt distortion, strength
+		/// can be configured with the slider.
+		/// </summary>
+		CrtDistortion
+	}
+}
